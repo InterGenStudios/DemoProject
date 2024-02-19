464bded3e7ea19eae4b83994fde5aa203842e9e2
InterGen <intergen@intergenstudios.com>
2024-02-19 07:46:56 -0600

    Test push of test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+name = input("input your name: ")
+bad_name= "cork"
+
+
+if bad_name in name:    
+    print("you are gay")
+
+
+
+
+mood = input("hello " + name + " how are you? ")
+good_mood = "good"
+bad_mood = " "
+
+if good_mood in mood:
+   print("cool... you're gay")
+else:
+    print("that sucks bro.....you're gay")
